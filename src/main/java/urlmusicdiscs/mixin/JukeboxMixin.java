e3f924d12a4815f1323c582b761e7c270962f309
TheModdersDen <site-admin@themoddersden.com>
2024-01-09 10:52:25 -0700

    Refactor sound loading and jukebox mixin

--- src/main/java/urlmusicdiscs/mixin/JukeboxMixin.java
+++ src/main/java/urlmusicdiscs/mixin/JukeboxMixin.java
@@ -2,24 +2,17 @@ package urlmusicdiscs.mixin;
 
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.JukeboxBlock;
 import net.minecraft.block.entity.JukeboxBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import urlmusicdiscs.URLMusicDiscs;
+
+import java.util.Objects;
 
 @Mixin(JukeboxBlockEntity.class)
 public class JukeboxMixin {
@@ -31,8 +24,8 @@ public class JukeboxMixin {
 		bufInfo.writeBlockPos(jukebox.getPos());
 		bufInfo.writeString("");
 
-		jukebox.getWorld().getPlayers().forEach(playerEntity1 -> {
-			ServerPlayNetworking.send((ServerPlayerEntity) playerEntity1, URLMusicDiscs.CUSTOM_RECORD_PACKET_ID, bufInfo);
-		});
-	}
+        for (PlayerEntity playerEntity1 : Objects.requireNonNull(jukebox.getWorld()).getPlayers()) {
+            ServerPlayNetworking.send((ServerPlayerEntity) playerEntity1, URLMusicDiscs.CUSTOM_RECORD_PACKET_ID, bufInfo);
+        }
+    }
 }
